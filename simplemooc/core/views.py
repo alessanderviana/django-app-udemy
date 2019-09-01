3df477dd67c32ea39e6586b58af749e6fad34202
alessanderviana <alessander.viana@hotmail.com>
2019-09-01 18:08:14 -0300

    Created the paths hello/ and world/

--- simplemooc/core/views.py
+++ simplemooc/core/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 def home(request):
     # return render(request, 'home.html', {'text': '<BR><BR>Hello<BR><BR>World<BR><BR>'})
     return HttpResponse('<H1><BR><BR><BR><BR>Hello, World!<BR><BR></H1>')
+
+def hello(request):
+        return HttpResponse('<H1><BR><BR><BR><BR><CENTER>Hello</CENTER></H1>')
+
+def world(request):
+    return HttpResponse('<H1><BR><BR><BR><BR><CENTER>World</CENTER></H1>')
